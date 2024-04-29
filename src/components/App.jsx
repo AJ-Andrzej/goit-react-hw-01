038de54f0bd2c2178d54fb342c32e20f3fcf06df
AJ-Andrzej <aayaschenko@gmail.com>
2024-04-29 19:44:28 +0200

    task-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import Profile from "./Profile/Profile"
+import FriendList from "./FriendList/FriendList"
 import userData from "../userData.json"
+import friends from "../friends.json"
 
 export default function App() {
     return (
         <>
-            <Profile user={userData} />
+        <Profile user={userData} />
+        <FriendList friends={friends} />
         </>
     )
 
