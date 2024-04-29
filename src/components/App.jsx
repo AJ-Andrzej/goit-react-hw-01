3042297fbd954502aa33f936c2ac738f96492dfc
AJ-Andrzej <aayaschenko@gmail.com>
2024-04-29 15:25:06 +0200

    task-1

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import Profile from "./Profile/Profile"
+import userData from "../userData.json"
+
+export default function App() {
+    return (
+        <>
+            <Profile user={userData} />
+        </>
+    )
+
+}
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,33 @@
+import css from "./Profile.module.css"
+export default function Profile({
+    user: {username, tag, location, avatar, stats: {followers, views, likes}}
+}) {
+    return (
+        <div className={css.container}>
+    <div className={css.baseInfomation}>
+    <img className={css.avatar}
+      src={avatar}
+      alt="User avatar"
+    />
+    <p className={css.name}>{username}</p>
+    <p className={css.info}>@{tag}</p>
+    <p className={css.info}>{location}</p>
+  </div>
+
+  <ul className={css.list}>
+    <li className={css.listItem}>
+      <span>Followers</span>
+      <span className={css.count}>{followers}</span>
+    </li>
+    <li className={css.listItem}>
+      <span>Views</span>
+      <span className={css.count}>{views}</span>
+    </li>
+    <li className={css.listItem}>
+      <span>Likes</span>
+      <span className={css.count}>{likes}</span>
+    </li>
+  </ul>
+        </div>
+    )
+}
